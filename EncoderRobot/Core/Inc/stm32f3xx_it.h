a20bf40b02afa9cd2429796de50410b0c20a26d4
JaviVi <jvicente@deusto.es>
2023-12-09 01:06:13 +0100

    a

--- EncoderRobot/Core/Inc/stm32f3xx_it.h
+++ EncoderRobot/Core/Inc/stm32f3xx_it.h
@@ -57,6 +57,7 @@ void PendSV_Handler(void);
 void SysTick_Handler(void);
 void EXTI3_IRQHandler(void);
 void EXTI4_IRQHandler(void);
+void CAN_RX0_IRQHandler(void);
 void EXTI9_5_IRQHandler(void);
 void TIM6_DAC1_IRQHandler(void);
 /* USER CODE BEGIN EFP */
--- EncoderRobot/Core/Src/stm32f3xx_it.c
+++ EncoderRobot/Core/Src/stm32f3xx_it.c
@@ -55,6 +55,7 @@
 /* USER CODE END 0 */
 
 /* External variables --------------------------------------------------------*/
+extern CAN_HandleTypeDef hcan;
 extern TIM_HandleTypeDef htim6;
 /* USER CODE BEGIN EV */
 
@@ -224,6 +225,20 @@ void EXTI4_IRQHandler(void)
   /* USER CODE BEGIN EXTI4_IRQn 1 */
 
   /* USER CODE END EXTI4_IRQn 1 */
+}
+
+/**
+  * @brief This function handles CAN RX0 interrupt.
+  */
+void CAN_RX0_IRQHandler(void)
+{
+  /* USER CODE BEGIN CAN_RX0_IRQn 0 */
+
+  /* USER CODE END CAN_RX0_IRQn 0 */
+  HAL_CAN_IRQHandler(&hcan);
+  /* USER CODE BEGIN CAN_RX0_IRQn 1 */
+
+  /* USER CODE END CAN_RX0_IRQn 1 */
 }
 
 /**
